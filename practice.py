65a1597f555326505b630b4a5fc4e2b64d61600d
SAKHAWATHALI <sakhawathali1@gmail.com>
2026-02-08 20:58:36 +0530

    intail commit

---
+++ practice.py
@@ -0,0 +1,16 @@
+# name  = input("what is your name?: ")
+# age = int(input("how old are you?:"))
+# print("my name is" ,name, "and I am", age)
+
+# numeric functions
+# print(max(1,2,3,4,5))
+# print(min(1,2,3,4,5))
+# print(pow(2,4))
+# print(abs(-3984))
+
+# # area of a right angle triangle
+# base = int(input("what is the base of the triangle?: "))
+# height = int(input("what is the height of the triangle?: "))
+# print("area of the right angle triangle",(1/2)*base*height)
+
+ 
